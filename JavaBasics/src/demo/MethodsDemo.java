918992420adeec1dc734f3e549b1ecbdda79db0a
Vijaykumar <viju451@gmail.com>
2022-05-26 09:25:39 +0530

    Adding new project Javabasics to repo

---
+++ JavaBasics/src/demo/MethodsDemo.java
@@ -0,0 +1,26 @@
+package demo;
+
+public class MethodsDemo {
+	
+	int num = 20;
+	String s = "variableType";
+	
+	public void data()
+	{
+		
+		int locdata = 20;
+		
+		System.out.println(num);
+		System.out.println("Am from classmethodsdemo");
+		System.out.println("Second statement");
+		//return 34;
+	}
+	
+	public void show()
+	{
+		System.out.println(s);
+		System.out.println("Second statement");
+		
+	}
+	
+}
